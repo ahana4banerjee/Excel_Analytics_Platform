a68ad43edd3932068d6a657d49bfba98fdf249e6
Ahana4Banerjee <banerjeeahana4@gmail.com>
2025-08-30 23:38:48 +0530

    login and signup created

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import connectDB from "./config/db.js";
+import userRoute from "./routes/auth.js";
 import dotenv from "dotenv";
 dotenv.config();
 
@@ -12,6 +13,7 @@ app.use(express.urlencoded({extended: true}));
 app.get("/", (req, res) => {
   res.send("Backend Running");
 });
+app.use('/user', userRoute);
 
 const PORT = 5000;
 app.listen(PORT, ()=>{
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,21 +1,38 @@
 import express from 'express';
-import { User } from '../models/user';
+import { User } from '../models/user.js';
 import bcrypt from 'bcryptjs';
+import jwt from "jsonwebtoken";
 
 const router = express.Router();
 
 router.post('/register', async (req, res) => {
-    const { name, email, password } = req.body;
     try{
+        const { name, email, password, role } = req.body;
+         
+        if (!name || !email || !password || !role) {
+            return res.status(400).json({
+                message: "Something is missing",
+                success: false
+            });
+        };
+
         const existingUser = await User.findOne({ email });
         if(existingUser){
-            return res.status(400).json({ message: "User already exists" });
+            return res.status(400).json({
+                message: "User already exists",
+                success: false
+            });
         }
 
         const hashedPassword = await bcrypt.hash(password, 10);
-        const newUser = new User({ name, email, password: hashedPassword });
+
+        const newUser = new User({ name, email, password: hashedPassword, role});
         await newUser.save();
-        res.status(201).json({ message: "User registered successfully" });
+
+        return res.status(201).json({ 
+            message: "User registered successfully",
+            success: true 
+        });
     }
     catch(error){
         res.status(500).json({ message: "Server error" });
@@ -23,20 +40,53 @@ router.post('/register', async (req, res) => {
 });
 
 router.post('/login', async (req, res) => {
-    const { email, password } = req.body;
+    
     try{
-        const user = await User.findOne({ email });
+        const { email, password, role } = req.body;
+
+        if (!email || !password || !role) {
+            return res.status(400).json({
+                message: "Something is missing",
+                success: false
+            });
+        };
+
+        let user = await User.findOne({ email });
         if(!user){
-            return res.status(400).json({ message: "Invalid email id!" });
+            return res.status(400).json({ 
+                message: "Invalid email id!",
+                success: false
+            });
         }
 
         const passwordMatch = await bcrypt.compare(password, user.password);
         if(!passwordMatch){
-            return res.status(400).json({ message: "Invalid Password!" });
+            return res.status(400).json({ 
+                message: "Invalid Password!",
+                success: false 
+            });
         }
-        res.status(200).json({ message: "Login successful" });
+
+        const tokenData = {
+            userId: user._id
+        }
+        const token = await jwt.sign(tokenData, process.env.SECRET_KEY, { expiresIn: '1d' });
+
+        user = {
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            role: user.role,
+        }
+
+        return res.status(200).cookie("token", token, { maxAge: 1 * 24 * 60 * 60 * 1000, httpsOnly: true, sameSite: 'strict' }).json({
+            message: `Login Successfull. Welcome back ${user.name}`,
+            user,
+            success: true
+        })
     }
     catch(error){
+        console.log(error)
         res.status(500).json({ message: "Server error" });
     }
 });
