47361f7843519f7712eee3960fc5615447198e6c
Ahana4Banerjee <banerjeeahana4@gmail.com>
2025-08-30 17:49:25 +0530

    mongodb setup

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,20 @@
 import express from "express";
 import cors from "cors";
+import connectDB from "./config/db.js";
+import dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 
 app.get("/", (req, res) => {
   res.send("Backend Running");
 });
 
 const PORT = 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, ()=>{
+    connectDB();
+    console.log(`server running at port ${PORT}`);
+})
